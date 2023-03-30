8c08144487ce9c5c5ba44af2bb9957aee29b7e05
david <dsxworld@gmail.com>
2023-03-30 22:54:43 +0200

    mejora pblucaciones y ajuste api

--- appSocial.js
+++ appSocial.js
@@ -1,35 +1,90 @@
-// let likes;
-// let contadorLikes = 0;
-// sumarLikes=()=>{
-//     likes=document.getElementById("likes");
-//     likes= parseInt(likes.textContent);
-//     contadorLikes=likes+1;
-//     likes=document.getElementById("likes")
-//     likes.textContent=contadorLikes
-// }
+const publicacionForm = document.getElementById('publicacion-form');
+const publicacionesContainer = document.getElementById('publicaciones-container');
 
-let contadorLikes;
-let haDadoLike = false;
-
-let likes = document.getElementById("likes");
-
-
-sumarLikes = () => {
-    contadorLikes = parseInt(likes.textContent);
+// Agregar evento submit al formulario
+publicacionForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevenir que el formulario se envíe automáticamente
     
-    if (!haDadoLike) {
-        contadorLikes += 1;
-        likes.textContent = contadorLikes;
-        likes.classList.add("liked");
-    } else {
-        contadorLikes -= 1;
-        likes.textContent = contadorLikes;
-        likes.classList.remove("liked");
+    const comentario = document.getElementById('comment').value; // Obtener el comentario ingresado
+    
+    // Crear un nuevo elemento de publicación y agregarlo al contenedor
+    const nuevaPublicacion = document.createElement('div');
+    nuevaPublicacion.classList.add('card', 'mt-4', 'bg-dark', 'text-white', 'w-100');
+    if (publicacionesContainer.firstChild) {
+        nuevaPublicacion.classList.add('mt-0');
     }
-    haDadoLike = !haDadoLike;
-};
+    nuevaPublicacion.innerHTML = `
+        <div class="card-body">
+            ${comentario}
+            <div class="d-flex justify-content-end align-items-center mt-3">
+                <button type="button" class="btn badge fa fa-heart me-2"><span> 0</span></button>
+                
+            </div>
+        </div>
+    `;
+    publicacionesContainer.appendChild(nuevaPublicacion);
+    
+    // Limpiar el campo de texto del formulario
+    document.getElementById('comment').value = '';
+    
+    // Agregar evento click al botón de "Me gusta"
+    const btnMeGusta = nuevaPublicacion.querySelector('.btn');
+    const contadorMeGusta = nuevaPublicacion.querySelector('.badge');
+    let numMeGusta = 0;
+    let haDadoMeGusta = false;
+    btnMeGusta.addEventListener('click', () => {
+        if (!haDadoMeGusta) {
+            numMeGusta++;
+            contadorMeGusta.innerText = numMeGusta;
+            btnMeGusta.classList.add('active');
+            haDadoMeGusta = true;
+        } else {
+            numMeGusta--;
+            contadorMeGusta.innerText = numMeGusta;
+            btnMeGusta.classList.remove('active');
+            haDadoMeGusta = false;
+        }
+    });
+});
 
-likes.addEventListener("click", sumarLikes);
 
 
-//BUSCAR TARGET EN DOCUMENTACIÓN
+
+
+const usersList = document.querySelector('.usersList');
+
+fetch('https://randomuser.me/api/?results=10')
+    .then(response => response.json())
+    .then(data => {
+    data.results.forEach(user => {
+        const userElement = document.createElement('div');
+        userElement.classList.add('container', 'bg-dark', 'border', 'border-success', 'rounded', 'mb-1', 'limited-width','mt-2');
+        userElement.innerHTML = `
+        <div class="row align-items-center">
+            <div class="col-lg-4">
+                <div class="ratio ratio-1x1">
+                <img src="${user.picture.large}" class="img-fluid rounded" alt="${user.name.first} ${user.name.last}">
+            </div>
+            </div>
+            <div class="col-lg-8">
+            <div class="row text-center">
+                <div class="col-12">
+                <p class="mb-0">${user.name.first} ${user.name.last}</p>
+                </div>
+                <div class="col-12">
+                <p class="mb-0">${user.location.city}, ${user.location.country}</p>
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col-6 btn-group p-1 mb-1">
+                <button type="submit" class="btn btn-outline-success w-100 fuente">Aceptar</button>
+                <button type="submit" class="btn btn-outline-success w-100 fuente">Ignorar</button>
+                </div>
+            </div>
+            </div>
+        </div>
+        `;
+        usersList.appendChild(userElement);
+    });
+    })
+    .catch(error => console.log(error));
