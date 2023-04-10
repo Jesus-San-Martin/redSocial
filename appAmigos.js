702541a6b152af50a112a496aee738936d1954fc
david <dsxworld@gmail.com>
2023-04-10 18:06:16 +0200

    anotaciones

--- appAmigos.js
+++ appAmigos.js
@@ -1,32 +1,36 @@
+// Seleccionar el contenedor donde se van a agregar los usuarios
 const usersList = document.querySelector('.usersList');
 
+// Hacer una solicitud GET a la API de Random User con un número específico de resultados
 fetch('https://randomuser.me/api/?results=20')
-    .then(response => response.json())
+    .then(response => response.json()) // Convertir la respuesta a formato JSON
     .then(data => {
-    data.results.forEach(user => {
-        const userElement = document.createElement('div');
-        userElement.classList.add('card', 'bg-dark', 'border', 'border-success', 'rounded','ajuste', 'm-2', 'text-cente','tamaño');
-        userElement.innerHTML = `
-        <div class="container align-items-center p-3">
-            <div class="row">
-                <div class="ratio ratio-1x1">
-                    <img src="${user.picture.large}" class="img-fluid rounded" alt="${user.name.first} ${user.name.last}">
-                </div>
-            </div>
-            <div class="col">
-                <div class="row text-center">
-                    <p class="mb-0">${user.name.first} ${user.name.last}</p>
-                    <p class="mb-0">${user.location.city}, ${user.location.country}</p>
-                </div>
-                <div class="row mt-2">
-                    <div class="col btn p-1">
-                        <a href="#" class="btn btn-danger">Amigo</a>
+        // Recorrer cada usuario obtenido de la respuesta
+        data.results.forEach(user => {
+            // Crear un nuevo elemento de usuario y agregarlo al contenedor
+            const userElement = document.createElement('div');
+            userElement.classList.add('card', 'bg-dark', 'border', 'border-success', 'rounded','ajuste', 'm-2', 'text-cente','tamaño');
+            userElement.innerHTML = `
+                <div class="container align-items-center p-3">
+                    <div class="row">
+                        <div class="ratio ratio-1x1">
+                            <img src="${user.picture.large}" class="img-fluid rounded" alt="${user.name.first} ${user.name.last}">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="row text-center">
+                            <p class="mb-0">${user.name.first} ${user.name.last}</p>
+                            <p class="mb-0">${user.location.city}, ${user.location.country}</p>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col btn p-1">
+                                <a href="#" class="btn btn-danger">Amigo</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        `;
-        usersList.appendChild(userElement);
-    });
+            `;
+            usersList.appendChild(userElement);
+        });
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error)); // Manejar cualquier error que ocurra durante la solicitud o el procesamiento de la respuesta
--- appSocial.js
+++ appSocial.js
@@ -1,18 +1,25 @@
+// --------------------------------------------------------------------------empieza publicaciones--------------------------------------------------
+// Obtener los elementos del DOM necesarios
 const publicacionForm = document.getElementById('publicacion-form');
 const publicacionesContainer = document.getElementById('publicaciones-container');
 
 // Agregar evento submit al formulario
 publicacionForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevenir que el formulario se envíe automáticamente
+    event.preventDefault(); // Prevenir que la pagina se recargue
     
-    const comentario = document.getElementById('comment').value; // Obtener el comentario ingresado
+    // Obtener el comentario ingresado
+    const comentario = document.getElementById('comment').value; 
     
     // Crear un nuevo elemento de publicación y agregarlo al contenedor
     const nuevaPublicacion = document.createElement('div');
     nuevaPublicacion.classList.add('card', 'mt-4', 'bg-dark', 'text-white', 'w-100');
+    
+    // Ajustar las clases del nuevo elemento según su posición en el contenedor
     if (publicacionesContainer.firstChild) {
         nuevaPublicacion.classList.add('mt-0');
     }
+    
+    // Agregar el HTML correspondiente al nuevo elemento de publicación
     nuevaPublicacion.innerHTML = `
         <div class="card-body mb-3">
             ${comentario}
@@ -25,6 +32,8 @@ publicacionForm.addEventListener('submit', (event) => {
             </div>
         </div>
     `;
+    
+    // Agregar el nuevo elemento de publicación al contenedor
     publicacionesContainer.appendChild(nuevaPublicacion);
     
     // Limpiar el campo de texto del formulario
@@ -37,11 +46,13 @@ publicacionForm.addEventListener('submit', (event) => {
     let haDadoMeGusta = false;
     btnMeGusta.addEventListener('click', () => {
         if (!haDadoMeGusta) {
+            // Aumentar el contador de "Me gusta"
             numMeGusta++;
             contadorMeGusta.innerText = numMeGusta;
             btnMeGusta.classList.add('active');
             haDadoMeGusta = true;
         } else {
+            // Disminuir el contador de "Me gusta"
             numMeGusta--;
             contadorMeGusta.innerText = numMeGusta;
             btnMeGusta.classList.remove('active');
@@ -50,44 +61,48 @@ publicacionForm.addEventListener('submit', (event) => {
     });
 });
 
-
-
-
-
+// -----------------------------------------------------------------Empieza teclers por el mundo-----------------------------------------------------
 const usersList = document.querySelector('.usersList');
 
+// Realizar una petición fetch a la API de randomuser.me para obtener 10 usuarios aleatorios
 fetch('https://randomuser.me/api/?results=10')
     .then(response => response.json())
     .then(data => {
-    data.results.forEach(user => {
-        const userElement = document.createElement('div');
-        userElement.classList.add('container', 'bg-dark', 'border', 'border-success', 'rounded', 'mb-1', 'limited-width','mt-2');
-        userElement.innerHTML = `
-        <div class="row align-items-center">
-            <div class="col-lg-4">
-                <div class="ratio ratio-1x1">
-                <img src="${user.picture.large}" class="img-fluid rounded" alt="${user.name.first} ${user.name.last}">
-            </div>
-            </div>
-            <div class="col-lg-8">
-            <div class="row text-center">
-                <div class="col-12">
-                <p class="mb-0">${user.name.first} ${user.name.last}</p>
+        // Iterar sobre la lista de usuarios obtenida y crear un elemento HTML para cada uno
+        data.results.forEach(user => {
+            // Crear un nuevo elemento HTML para el usuario
+            const userElement = document.createElement('div');
+            // Agregar clases CSS al elemento para darle estilo
+            userElement.classList.add('container', 'bg-dark', 'border', 'border-success', 'rounded', 'mb-1', 'limited-width','mt-2');
+            // Agregar contenido HTML al elemento usando la información del usuario obtenida de la API
+            userElement.innerHTML = `
+                <div class="row align-items-center">
+                    <div class="col-lg-4">
+                        <div class="ratio ratio-1x1">
+                            <img src="${user.picture.large}" class="img-fluid rounded" alt="${user.name.first} ${user.name.last}">
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="row text-center">
+                            <div class="col-12">
+                                <p class="mb-0">${user.name.first} ${user.name.last}</p>
+                            </div>
+                            <div class="col-12">
+                                <p class="mb-0">${user.location.city}, ${user.location.country}</p>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-6 btn-group p-1 mb-1">
+                                <button type="submit" class="btn btn-outline-success w-100 fuente">Aceptar</button>
+                                <button type="submit" class="btn btn-outline-success w-100 fuente">Ignorar</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-12">
-                <p class="mb-0">${user.location.city}, ${user.location.country}</p>
-                </div>
-            </div>
-            <div class="row mt-2">
-                <div class="col-6 btn-group p-1 mb-1">
-                <button type="submit" class="btn btn-outline-success w-100 fuente">Aceptar</button>
-                <button type="submit" class="btn btn-outline-success w-100 fuente">Ignorar</button>
-                </div>
-            </div>
-            </div>
-        </div>
-        `;
-        usersList.appendChild(userElement);
-    });
+            `;
+            // Agregar el elemento HTML del usuario creado a la lista de usuarios
+            usersList.appendChild(userElement);
+        });
     })
     .catch(error => console.log(error));
+// -----------------------------------------------------------------Empieza busqueda de publicaciones por nombre de us-----------------------------------------------------
